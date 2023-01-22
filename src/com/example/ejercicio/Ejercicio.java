4bbb819c39323873c7327a4d1b17cda05ce38b8c
ErickBrayan <ebrayan2000@gmail.com>
2023-01-21 23:51:36 -0500

    commit Ejercicio1

---
+++ src/com/example/ejercicio/Ejercicio.java
@@ -0,0 +1,42 @@
+package com.example.ejercicio;
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+        byte byteNumber = 12;
+        System.out.println("byteNumber = " + byteNumber);
+
+        short shortNumber = 100;
+        System.out.println("shortNumber = " + shortNumber);
+
+        int intNumber = 1000;
+        System.out.println("intNumber = " + intNumber);
+
+        long longNumber = 10000;
+        System.out.println("longNumber = " + longNumber);
+
+
+
+
+        float floatNumber = 12.36f;
+        System.out.println("floatNumber = " + floatNumber);
+
+        double doubleNumber = 12.45;
+        System.out.println("doubleNumber = " + doubleNumber);
+
+
+
+
+
+
+        char charText = 'a';
+        System.out.println("charText = " + charText);
+
+        String stringText = "hola";
+        System.out.println("stringText = " + stringText);
+
+
+
+    }
+}
